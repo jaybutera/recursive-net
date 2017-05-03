b52d456b8dc68797fc7f5c968fffd87188d946ca
Jay Butera <buterajay@gmail.com>
2017-05-03 00:17:15 -0500

    progress

--- recurs_net.py
+++ recurs_net.py
@@ -15,11 +15,11 @@ class ARN (RecNet):
         size = 3
 
         # Weight matrices
-        self.W_i  = np.random.rand(size*size,size), dtype=np.float32)
-        self.W_ih = np.random.rand((size,size), dtype=np.float32)
-        self.W_ho = np.random.rand((size,size), dtype=np.float32)
+        self.W_i  = np.random.rand(size,size*size)
+        self.W_ih = np.random.rand(size,size)
+        self.W_ho = np.random.rand(size,size)
 
-        if level < 1: # Base case
+        if level <= 1: # Base case
             self.input_nodes  = [BRN(nodes) for i in range(nodes)]
             self.hidden_nodes = [BRN(nodes) for i in range(nodes)]
             self.output_nodes = [BRN(nodes) for i in range(nodes)]
@@ -33,12 +33,12 @@ class ARN (RecNet):
 
     def activate (self, x):
         # Compute input layer
-        inp_inputs = np.dot(self.W_i, x)
-        i = np.array([ x.activate(inp_inputs) for x in self.input_nodes ])
+        #inp_inputs = np.dot(self.W_i, x)
+        i = np.array([ n.activate(x) for n in self.input_nodes ])
 
         # Compute hidden layer
-        hid_inputs = np.dot(self.W_ih, inps)
-        h = np.array([ x.activate(hid_inputs) for x in self.hidden_nodes ])
+        hid_inputs = np.dot(self.W_ih, i)
+        h = np.array([ n.activate(hid_inputs) for n in self.hidden_nodes ])
 
         # Compute output layer
         out_inputs = np.dot(self.W_ho, h)
@@ -49,10 +49,12 @@ class ARN (RecNet):
 # Base node
 class BRN (RecNet):
     def __init__ (self, nodes):
+        size = 3
+
         # Weight matrices
-        self.W_i  = np.random.rand((size*size,size), dtype=np.float32)
-        self.W_ih = np.random.rand((size,size), dtype=np.float32)
-        self.W_ho = np.random.rand((size,size), dtype=np.float32)
+        self.W_i  = np.random.rand(size,size*size)
+        self.W_ih = np.random.rand(size,size)
+        self.W_ho = np.random.rand(size,size)
 
     def sigmoid (self, x):
         return 1. / (1. + np.exp(-x))
@@ -60,10 +62,10 @@ class BRN (RecNet):
     def activate (self, x):
         i = self.sigmoid( np.dot(self.W_i, x) )
         h = self.sigmoid( np.dot(self.W_ih, i) )
-        o = self.sigmoid( np.dot(self.W_ho, o) )
+        o = self.sigmoid( np.dot(self.W_ho, h) )
 
-        return o
+        return o.flatten()
 
 if __name__ == '__main__':
     nn = ARN(3, 2)
-    nn.activate(
+    nn.activate( np.random.rand(9,1) )
